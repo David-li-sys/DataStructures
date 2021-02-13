7860a41a5b7ef48ecddf38d59619c599f10740a8
David-li-sys <508727912@qq.com>
2021-02-13 20:56:48 +0800

    插入排序

---
+++ src/com/njsf/F_C_InsertSort.java
@@ -0,0 +1,30 @@
+package com.njsf;
+
+import java.util.Arrays;
+
+public class F_C_InsertSort {
+    public static void main(String[] args) {
+        int[] arr = new int[80000];
+        for (int i = 0; i < 80000; i++) {
+            arr[i] = (int)(Math.random()*80000000);
+        }
+        Long start = System.currentTimeMillis();
+        insertSort(arr);
+        Long end = System.currentTimeMillis();
+        System.out.println(end - start);
+    }
+    public static void insertSort(int[] arr){
+        for(int i = 1;i<arr.length;i++){
+            int insertVal = arr[i];//记录需要插入的数
+            int insertIndex = i-1;//前一个数索引
+            while(insertIndex >= 0 && insertVal < arr[insertIndex]){
+                //不满足就后移
+                arr[insertIndex+1] = arr[insertIndex];
+                insertIndex--;
+            }
+            //插入
+            arr[insertIndex+1] = insertVal;
+        }
+
+    }
+}
