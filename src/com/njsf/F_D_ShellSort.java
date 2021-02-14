acbc77f19527ac69d580b3a8397c08f5206702c0
David-li-sys <508727912@qq.com>
2021-02-14 09:22:35 +0800

    希尔排序两种实现

--- src/com/njsf/F_D_ShellSort.java
+++ src/com/njsf/F_D_ShellSort.java
@@ -4,14 +4,18 @@ import java.util.Arrays;
 
 public class F_D_ShellSort {
     public static void main(String[] args) {
-        int[] arr = {3,6,2,9,1};
-        System.out.println(Arrays.toString(arr));
-        System.out.println("====================================");
-        test(arr);
+        int[] arr = new int[80000];
+        for (int i = 0; i < 80000; i++) {
+            arr[i] = (int)(Math.random()*80000000);
+        }
+        Long start = System.currentTimeMillis();
+        shellSort(arr);
+        Long end = System.currentTimeMillis();
+        System.out.println(end - start);
 
 
     }
-
+    //交换法
     public static void shellSort(int[] arr){
 
         int temp = 0;
@@ -22,6 +26,8 @@ public class F_D_ShellSort {
                         temp = arr[j];
                         arr[j] = arr[j+gap];
                         arr[j+gap] = temp;
+                    }else{
+                        break;
                     }
 
                 }
@@ -30,6 +36,24 @@ public class F_D_ShellSort {
 
 
     }
+    //插入法
+    public static void shellSort2(int[] arr){
+        for(int gap = arr.length/2; gap > 0; gap /= 2){
+            for(int i = gap; i < arr.length; i++){
+                int j = i;
+                int temp = arr[j];
+                if(arr[j] < arr[j-gap]){
+                    while(j - gap >= 0 && temp < arr[j - gap]){
+                        arr[j] = arr[j-gap];
+                        j -= gap;
+                    }
+                    arr[j] = temp;
+                }
+            }
+        }
+    }
+
+
     public static void test(int[] arr){
         int temp;
         for(int i = 1;i<arr.length; i++){
