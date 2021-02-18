610bc2410e515b5aa9ccbefc224a96d47fb329c8
David-li-sys <508727912@qq.com>
2021-02-18 17:31:18 +0800

    汉诺塔

---
+++ src/com/njsf/algorithm/Hanoitiwer.java
@@ -0,0 +1,17 @@
+package com.njsf.algorithm;
+
+public class Hanoitiwer {
+    public static void main(String[] args) {
+        hanoiTower(5, 'A', 'B', 'C');
+    }
+
+    public static void hanoiTower(int num, char a, char b, char c){
+        if(num == 1){
+            System.out.println("第1个盘从 " + a + "->"+c);
+        }else{
+            hanoiTower(num - 1, a, c, b);
+            System.out.println("第"+ num + "个盘从 " + a + "->" + c);
+            hanoiTower(num - 1, b, c, a);
+        }
+    }
+}
